eb89ac934e488b5f50d153cad8725444002d2a42
Stéphane Mourey <steph@stephanemourey.fr>
2019-04-30 15:26:31 +0200

    Adding first test SubItemIsCreated

--- src/tests/itemFactoryTest.php
+++ src/tests/itemFactoryTest.php
@@ -1,5 +1,16 @@
 <?php
 use PHPUnit\Framework\TestCase;
+use OnePress\Db\Item;
+use OnePress\Db\ItemFactory;
+
+class SubItem extends Item {}
+class SubSubItem extends SubItem {}
+
 
 class ItemFactoryTest extends TestCase {
+	public function testSubItemIsCreated() {
+		$factory = new ItemFactory;
+		$this->assertInstanceOf('SubItem',$factory->getNew($this->di,'SubItem');
+	}
 }
+~
