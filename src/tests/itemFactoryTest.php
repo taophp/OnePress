0409077a2e9ea15e91ea588399aeb081ff3903ab
Stéphane Mourey <steph@stephanemourey.fr>
2019-04-30 17:54:34 +0200

    Working on tests

--- src/tests/itemFactoryTest.php
+++ src/tests/itemFactoryTest.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 use PHPUnit\Framework\TestCase;
 use OnePress\Db\Item;
 use OnePress\Db\ItemFactory;
@@ -8,6 +10,11 @@ class SubSubItem extends SubItem {}
 
 
 class ItemFactoryTest extends TestCase {
+	protected $di;
+	protected function setUp() {
+		$this->di = new FactoryDefault();
+	}
+
 	public function testSubItemIsCreated() {
 		$factory = new ItemFactory($this->di);
 		$this->assertInstanceOf('SubItem',$factory->getNew($this->di,'SubItem'));
